ea795488e9fbbae33b76fb83a261f2e021ad6dd2
Rodrigo <modrips4@gmail.com>
2025-06-12 16:17:05 -0300

    Arreglos del documento por los nuevos cambios

--- Obligatorio_PrimeraEntrega/ObligatorioDB2.sql
+++ Obligatorio_PrimeraEntrega/ObligatorioDB2.sql
@@ -34,7 +34,6 @@ CREATE TABLE Modulos (
     Titulo VARCHAR(50) NOT NULL,
     Descripcion VARCHAR(MAX) NOT NULL,
     Orden INT NOT NULL,
-	Activo BIT NOT NULL CHECK (Activo IN(0,1)) DEFAULT 1,
     FOREIGN KEY (CursoID) REFERENCES Cursos(CursoID)
 );
 GO
@@ -45,7 +44,6 @@ CREATE TABLE Clases (
     Titulo VARCHAR(50) NOT NULL,
     Descripcion VARCHAR(MAX) NOT NULL,
 	Orden INT NOT NULL,
-	Activo BIT CHECK(Activo IN(0,1)) NOT NULL DEFAULT 1,
     FOREIGN KEY (ModuloID) REFERENCES Modulos(ModuloID)
 );
 GO
@@ -122,94 +120,94 @@ GO
 
  --/////// CREACION DE DATOS /////// --
 
--- DOCENTES
-INSERT INTO Docentes (Nombre, Apellido, Correo, FechaRegistro, Activo) VALUES
-('María', 'González', 'maria.gonzalez@edu.com', '2024-01-01', 1),
-('José', 'Fernández', 'jose.fernandez@edu.com', '2024-01-02', 1);
+---- DOCENTES
+--INSERT INTO Docentes (Nombre, Apellido, Correo, FechaRegistro, Activo) VALUES
+--('María', 'González', 'maria.gonzalez@edu.com', '2024-01-01', 1),
+--('José', 'Fernández', 'jose.fernandez@edu.com', '2024-01-02', 1);
 
--- CURSOS
-INSERT INTO Cursos (DocenteID, Titulo, Descripcion, Duracion, Activo, NotaMinima) VALUES
-(1, 'Curso de Matemáticas Básicas', 'Curso introductorio a las matemáticas', 60, 1, 70.00),
-(2, 'Curso de Física General', 'Curso básico de física aplicada', 60, 1, 70.00);
+---- CURSOS
+--INSERT INTO Cursos (DocenteID, Titulo, Descripcion, Duracion, Activo, NotaMinima) VALUES
+--(1, 'Curso de Matemáticas Básicas', 'Curso introductorio a las matemáticas', 60, 1, 70.00),
+--(2, 'Curso de Física General', 'Curso básico de física aplicada', 60, 1, 70.00);
 
--- MODULOS
-INSERT INTO Modulos (CursoID, Titulo, Descripcion, Orden) VALUES
-(1, 'Álgebra Básica', 'Fundamentos del álgebra', 1),
-(1, 'Geometría', 'Estudio de las figuras geométricas', 2),
-(1, 'Trigonometría', 'Introducción a las funciones trigonométricas', 3),
-(2, 'Cinemática', 'Estudio del movimiento', 1),
-(2, 'Dinámica', 'Fuerzas y movimiento', 2),
-(2, 'Termodinámica', 'Estudio del calor y energía', 3);
+---- MODULOS
+--INSERT INTO Modulos (CursoID, Titulo, Descripcion, Orden) VALUES
+--(1, 'Álgebra Básica', 'Fundamentos del álgebra', 1),
+--(1, 'Geometría', 'Estudio de las figuras geométricas', 2),
+--(1, 'Trigonometría', 'Introducción a las funciones trigonométricas', 3),
+--(2, 'Cinemática', 'Estudio del movimiento', 1),
+--(2, 'Dinámica', 'Fuerzas y movimiento', 2),
+--(2, 'Termodinámica', 'Estudio del calor y energía', 3);
 
-INSERT INTO Clases (ModuloID, Titulo, Descripcion, Orden, Activo) VALUES
-(1,'Clase 1.1: Introducción al Álgebra','Conceptos básicos de álgebra',1,1),
-(1,'Clase 1.2: Ecuaciones Lineales','Resolución de ecuaciones lineales',2,1),
-(1,'Clase 1.3: Sistema de Ecuaciones','Métodos para resolver sistemas',3,1),
-(2,'Clase 2.1: Figuras Geométricas','Introducción a la geometría',1,1),
-(2,'Clase 2.2: Áreas y Volúmenes','Cálculo de áreas y volúmenes',2,1),
-(2,'Clase 2.3: Teorema de Pitágoras','Aplicaciones del teorema de Pitágoras',3,1),
-(3,'Clase 3.1: Funciones Trigonométricas','Estudio de seno, coseno y tangente',1,1),
-(3,'Clase 3.2: Identidades Trigonométricas','Identidades y ecuaciones',2,1),
-(3,'Clase 3.3: Aplicaciones de la Trigonometría','Usos de la trigonometría',3,1),
-(4,'Clase 4.1: Movimiento Rectilíneo','Estudio del movimiento rectilíneo',1,1),
-(4,'Clase 4.2: Leyes de Newton','Introducción a las leyes de Newton',2,1),
-(4,'Clase 4.3: Velocidad y Aceleración','Cálculo de velocidad y aceleración',3,1),
-(5,'Clase 5.1: Ley de la Gravedad','Gravedad y fuerzas de atracción',1,1),
-(5,'Clase 5.2: Fuerzas de Fricción','Fuerzas que afectan el movimiento',2,1),
-(5,'Clase 5.3: Trabajo y Energía','Conceptos de trabajo y energía',3,1),
-(6,'Clase 6.1: Leyes de la Termodinámica','Introducción a la termodinámica',1,1),
-(6,'Clase 6.2: Ciclo Termodinámico','Estudio de los ciclos termodinámicos',2,1),
-(6,'Clase 6.3: Transferencia de Calor','Mecanismos de transferencia de calor',3,1);
+--INSERT INTO Clases (ModuloID, Titulo, Descripcion, Orden, Activo) VALUES
+--(1,'Clase 1.1: Introducción al Álgebra','Conceptos básicos de álgebra',1,1),
+--(1,'Clase 1.2: Ecuaciones Lineales','Resolución de ecuaciones lineales',2,1),
+--(1,'Clase 1.3: Sistema de Ecuaciones','Métodos para resolver sistemas',3,1),
+--(2,'Clase 2.1: Figuras Geométricas','Introducción a la geometría',1,1),
+--(2,'Clase 2.2: Áreas y Volúmenes','Cálculo de áreas y volúmenes',2,1),
+--(2,'Clase 2.3: Teorema de Pitágoras','Aplicaciones del teorema de Pitágoras',3,1),
+--(3,'Clase 3.1: Funciones Trigonométricas','Estudio de seno, coseno y tangente',1,1),
+--(3,'Clase 3.2: Identidades Trigonométricas','Identidades y ecuaciones',2,1),
+--(3,'Clase 3.3: Aplicaciones de la Trigonometría','Usos de la trigonometría',3,1),
+--(4,'Clase 4.1: Movimiento Rectilíneo','Estudio del movimiento rectilíneo',1,1),
+--(4,'Clase 4.2: Leyes de Newton','Introducción a las leyes de Newton',2,1),
+--(4,'Clase 4.3: Velocidad y Aceleración','Cálculo de velocidad y aceleración',3,1),
+--(5,'Clase 5.1: Ley de la Gravedad','Gravedad y fuerzas de atracción',1,1),
+--(5,'Clase 5.2: Fuerzas de Fricción','Fuerzas que afectan el movimiento',2,1),
+--(5,'Clase 5.3: Trabajo y Energía','Conceptos de trabajo y energía',3,1),
+--(6,'Clase 6.1: Leyes de la Termodinámica','Introducción a la termodinámica',1,1),
+--(6,'Clase 6.2: Ciclo Termodinámico','Estudio de los ciclos termodinámicos',2,1),
+--(6,'Clase 6.3: Transferencia de Calor','Mecanismos de transferencia de calor',3,1);
 
--- ESTUDIANTES
-INSERT INTO Estudiantes (Nombre, Apellido, Correo) VALUES
-('Juan', 'Pérez', 'juan.perez@example.com'),
-('Ana', 'López', 'ana.lopez@example.com'),
-('Carlos', 'Martínez', 'carlos.martinez@example.com');
+---- ESTUDIANTES
+--INSERT INTO Estudiantes (Nombre, Apellido, Correo) VALUES
+--('Juan', 'Pérez', 'juan.perez@example.com'),
+--('Ana', 'López', 'ana.lopez@example.com'),
+--('Carlos', 'Martínez', 'carlos.martinez@example.com');
 
--- ESTUDIANTESMODULOS
-INSERT INTO EstudiantesModulos (ModuloID, EstudianteID, NotaExamen) VALUES
-(1, 1, 85.00), (2, 1, 80.00), (3, 1, 90.00),
-(4, 1, 95.00), (5, 1, 85.00), (6, 1, 90.00),
-(1, 2, 65.00), (2, 2, 70.00), (3, 2, 75.00),
-(1, 3, 50.00), (2, 3, 60.00), (3, 3, 55.00),
-(4, 3, 45.00), (5, 3, 50.00), (6, 3, 40.00);
+---- ESTUDIANTESMODULOS
+--INSERT INTO EstudiantesModulos (ModuloID, EstudianteID, NotaExamen) VALUES
+--(1, 1, 85.00), (2, 1, 80.00), (3, 1, 90.00),
+--(4, 1, 95.00), (5, 1, 85.00), (6, 1, 90.00),
+--(1, 2, 65.00), (2, 2, 70.00), (3, 2, 75.00),
+--(1, 3, 50.00), (2, 3, 60.00), (3, 3, 55.00),
+--(4, 3, 45.00), (5, 3, 50.00), (6, 3, 40.00);
 
--- ESTUDIANTESCLASES
-INSERT INTO EstudiantesClases (ClaseID, EstudianteID, Visto) VALUES
--- Estudiante 1
-(1, 1, 1), (2, 1, 1), (3, 1, 1), (4, 1, 1), (5, 1, 1), (6, 1, 1),
-(7, 1, 1), (8, 1, 1), (9, 1, 1), (10, 1, 1), (11, 1, 1), (12, 1, 1),
-(13, 1, 1), (14, 1, 1), (15, 1, 1), (16, 1, 1), (17, 1, 1), (18, 1, 1),
--- Estudiante 2
-(1, 2, 1), (2, 2, 1), (3, 2, 1), (4, 2, 1), (5, 2, 1), (6, 2, 1),
-(7, 2, 0), (8, 2, 0), (9, 2, 0), (10, 2, 0), (11, 2, 0), (12, 2, 0),
-(13, 2, 0), (14, 2, 0), (15, 2, 0), (16, 2, 0), (17, 2, 0), (18, 2, 0),
--- Estudiante 3
-(1, 3, 1), (2, 3, 1), (3, 3, 1), (4, 3, 1), (5, 3, 1), (6, 3, 1),
-(7, 3, 1), (8, 3, 1), (9, 3, 1), (10, 3, 1), (11, 3, 1), (12, 3, 1),
-(13, 3, 1), (14, 3, 1), (15, 3, 1), (16, 3, 1), (17, 3, 1), (18, 3, 1);
+---- ESTUDIANTESCLASES
+--INSERT INTO EstudiantesClases (ClaseID, EstudianteID, Visto) VALUES
+---- Estudiante 1
+--(1, 1, 1), (2, 1, 1), (3, 1, 1), (4, 1, 1), (5, 1, 1), (6, 1, 1),
+--(7, 1, 1), (8, 1, 1), (9, 1, 1), (10, 1, 1), (11, 1, 1), (12, 1, 1),
+--(13, 1, 1), (14, 1, 1), (15, 1, 1), (16, 1, 1), (17, 1, 1), (18, 1, 1),
+---- Estudiante 2
+--(1, 2, 1), (2, 2, 1), (3, 2, 1), (4, 2, 1), (5, 2, 1), (6, 2, 1),
+--(7, 2, 0), (8, 2, 0), (9, 2, 0), (10, 2, 0), (11, 2, 0), (12, 2, 0),
+--(13, 2, 0), (14, 2, 0), (15, 2, 0), (16, 2, 0), (17, 2, 0), (18, 2, 0),
+---- Estudiante 3
+--(1, 3, 1), (2, 3, 1), (3, 3, 1), (4, 3, 1), (5, 3, 1), (6, 3, 1),
+--(7, 3, 1), (8, 3, 1), (9, 3, 1), (10, 3, 1), (11, 3, 1), (12, 3, 1),
+--(13, 3, 1), (14, 3, 1), (15, 3, 1), (16, 3, 1), (17, 3, 1), (18, 3, 1);
 
--- INSCRIPCION
-INSERT INTO Inscripcion (EstudianteID, CursoID, FechaInscripcion) VALUES
-(1, 1, '2024-02-01'),
-(2, 1, '2024-02-02'),
-(3, 2, '2024-02-03');
+---- INSCRIPCION
+--INSERT INTO Inscripcion (EstudianteID, CursoID, FechaInscripcion) VALUES
+--(1, 1, '2024-02-01'),
+--(2, 1, '2024-02-02'),
+--(3, 2, '2024-02-03');
 
--- PROGRESO
-INSERT INTO Progreso (EstudianteID, CursoID, PorcentajeAvance) VALUES
-(1, 1, 100.00),
-(2, 1, 50.00),
-(3, 2, 40.00);
+---- PROGRESO
+--INSERT INTO Progreso (EstudianteID, CursoID, PorcentajeAvance) VALUES
+--(1, 1, 100.00),
+--(2, 1, 50.00),
+--(3, 2, 40.00);
 
--- CALIFICACION
-INSERT INTO Calificacion (EstudianteID, CursoID, Nota) VALUES
-(1, 1, 87.5),
-(3, 2, 50.0);
+---- CALIFICACION
+--INSERT INTO Calificacion (EstudianteID, CursoID, Nota) VALUES
+--(1, 1, 87.5),
+--(3, 2, 50.0);
 
--- CERTIFICACION
-INSERT INTO Certificacion (EstudianteID, CursoID, FechaEmision) VALUES
-(1, 1, '2024-03-01');
+---- CERTIFICACION
+--INSERT INTO Certificacion (EstudianteID, CursoID, FechaEmision) VALUES
+--(1, 1, '2024-03-01');
 -----------------------------------------------------------------------------------------
 
 --  Stored Procedure para nuevo alumno
